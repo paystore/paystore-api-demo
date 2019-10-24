a7531a0b865bbba079903268c238d9ffc62daeff
thalles.rafael <thalles.rafael@phoebustecnologia.com.br>
2019-10-24 16:27:32 -0300

    Adicionando exemplo de resolução de pendencias; adicionando metodo para definir app como principal

--- app/src/main/java/br/com/phoebus/payments/demo/MainActivity.java
+++ app/src/main/java/br/com/phoebus/payments/demo/MainActivity.java
@@ -1,11 +1,23 @@
 package br.com.phoebus.payments.demo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import br.com.phoebus.android.payments.api.Payment;
+import br.com.phoebus.android.payments.api.PaymentStatus;
+import br.com.phoebus.android.payments.api.exception.ClientException;
+import br.com.phoebus.android.payments.api.provider.PaymentContract;
+import br.com.phoebus.android.payments.api.provider.PaymentProviderApi;
+import br.com.phoebus.android.payments.api.provider.PaymentProviderRequest;
+import br.com.phoebus.payments.demo.utils.CredentialsUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -83,5 +95,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTheme(View view) {
         startActivity(new Intent(this, SetThemeActivity.class));
+    }
+
+    public void solvePendencies(View view) {
+        try {
+            List<Payment> paymentList;
+            Boolean pendingsFound = false;
+            PaymentProviderRequest request = new PaymentProviderRequest(CredentialsUtils.getMyAppInfo(this.getPackageManager(), this.getPackageName()), new Date());
+
+            request.setColumns(new String[]{
+                    PaymentContract.column.ID,
+                    PaymentContract.column.PAYMENT_STATUS
+            });
+
+            paymentList = PaymentProviderApi.create(this).findAll(request);
+            for (final Payment payment : paymentList) {
+                if (payment.getPaymentStatus() == PaymentStatus.PENDING) {
+                    pendingsFound = true;
+                    startActivity(new Intent(this, ConfirmPaymentActivity.class).putExtra(MainActivity.EXTRA_PAYMENT_ID, payment.getPaymentId()));
+                }
+            }
+            if (pendingsFound) {
+                Toast.makeText(getApplicationContext(), "Nenhuma pendência encontrada!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Todas as pendêcias foram resolvidas!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (PackageManager.NameNotFoundException | ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setMainApp(View view) {
+        startActivity(new Intent(this, SetMainAppActivity.class));
     }
 }
---
+++ app/src/main/java/br/com/phoebus/payments/demo/SetMainAppActivity.java
@@ -0,0 +1,71 @@
+package br.com.phoebus.payments.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.phoebus.android.payments.api.ErrorData;
+import br.com.phoebus.android.payments.api.PaymentClient;
+import br.com.phoebus.android.payments.api.exception.ClientException;
+
+public class SetMainAppActivity extends AppCompatActivity {
+
+    private PaymentClient paymentClient;
+
+    private EditText packageNameEdt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle(R.string.setThemeBtn);
+        setContentView(R.layout.activity_set_main_app);
+
+        this.packageNameEdt = ((EditText) this.findViewById(R.id.packageNameEdt));
+
+        this.packageNameEdt.setText("br.com.phoebus.payments.demo");
+
+        this.paymentClient = new PaymentClient();
+        this.paymentClient.bind(this.getApplicationContext());
+    }
+
+    public void doSetMainApp(View view) {
+
+        if (!isDataValid()) return;
+
+        try {
+            this.paymentClient.setMainApp(packageNameEdt.getText().toString(), new PaymentClient.PaymentCallback() {
+                @Override
+                public void onSuccess(Object data) {
+
+                    Toast.makeText(SetMainAppActivity.this, "App principal definido!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(ErrorData errorData) {
+                    Toast.makeText(SetMainAppActivity.this, "Erro ao definir app principal: " + errorData.getPaymentsResponseCode() +
+                            " = " + errorData.getResponseMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (ClientException e) {
+            e.printStackTrace();
+            Toast.makeText(SetMainAppActivity.this, "Falha na chamada do serviço: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    private boolean isDataValid() {
+
+        boolean ret = true;
+
+        if ("".equals(this.packageNameEdt.getText().toString())) {
+            this.packageNameEdt.setError(getString(R.string.requieredFieldError));
+            ret = false;
+        }
+
+        return ret;
+    }
+
+}
